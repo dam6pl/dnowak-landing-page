8c58e1247e18b906dca315eab78d4a8df2e38e65
Damian Nowak <me@dnowak.dev>
2019-09-13 23:12:33 +0200

    google api keys

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,8 @@
 
     var $form = $('form');
 
-    grecaptcha.ready(function() {
-        grecaptcha.execute('6LePJZgUAAAAAGjoi2wG1-wr0yoK4J6HGBwRy6qW', {action: 'contact_form'}).then(function(token) {
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6LelXLgUAAAAANdJ9h5jZqn8Rq2JCDdMHuXbOBgw', { action: 'contact_form' }).then(function (token) {
             $form.prepend('<input type="hidden" name="token" value="' + token + '">');
             $form.prepend('<input type="hidden" name="action" value="contact_form">');
         });
@@ -20,19 +20,19 @@
                 type: 'POST',
                 url: "",
                 data: formValues
-            }).done( function (response) {
+            }).done(function (response) {
                 $form.trigger('reset');
 
                 if (response === '1') {
                     $('.return-message span.success').removeClass('d-none');
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('.return-message span.success').addClass('d-none');
-                    }, 5000)
+                    }, 5000);
                 } else {
                     $('.return-message span.error').removeClass('d-none');
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('.return-message span.error').addClass('d-none');
-                    }, 5000)
+                    }, 5000);
                 }
             });
         });
