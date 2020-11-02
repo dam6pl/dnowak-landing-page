6f56b38b89d4773373d5f19c7fba9b4c953754fd
dam6pl <dam6pl@users.noreply.github.com>
2020-11-02 09:31:58 +0000

    deployed via Gatsby Publish Action 🎩 for e9120e8ff941d59763a479c6273783af04fe4a0d

---
+++ component---src-pages-404-js-e279b7e1561cc2e3362f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,o,w){"use strict";w.r(o);o.default=function(){return"undefined"!=typeof window&&(window.location="/"),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e279b7e1561cc2e3362f.js.map
